7423f3ef9ac821fdc6365017074b85441723ff31
salimgumar <salimgumar12@gmail.com>
2025-07-10 12:48:10 +0500

    Add files via upload

---
+++ data.js
@@ -0,0 +1,311 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-master-bedroom-2-panorama",
+      "name": "master bedroom 2 panorama",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9009756717997366,
+          "pitch": 0.5936976310589959,
+          "rotation": 0,
+          "target": "1-master-bedroom-2-panorama-2"
+        },
+        {
+          "yaw": -2.0833347337493944,
+          "pitch": 0.3195558030627801,
+          "rotation": 0,
+          "target": "5-bedroom-panorama-2"
+        },
+        {
+          "yaw": -2.0880788546605267,
+          "pitch": 0.13799353668368042,
+          "rotation": 0,
+          "target": "2-master-bedroom-panorama-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-master-bedroom-2-panorama-2",
+      "name": "master bedroom 2 panorama 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6596173713318016,
+          "pitch": 0.5340592089827894,
+          "rotation": 0,
+          "target": "0-master-bedroom-2-panorama"
+        },
+        {
+          "yaw": 1.8469947696776217,
+          "pitch": 0.8260934070723192,
+          "rotation": 0,
+          "target": "5-bedroom-panorama-2"
+        },
+        {
+          "yaw": 1.8959316476804737,
+          "pitch": 0.6134906896111154,
+          "rotation": 0,
+          "target": "2-master-bedroom-panorama-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-master-bedroom-panorama-2",
+      "name": "master bedroom panorama 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.2987204194092179,
+          "pitch": 0.5405146368707143,
+          "rotation": 0,
+          "target": "3-master-bedroom-panorama"
+        },
+        {
+          "yaw": -1.7586336885105212,
+          "pitch": 0.47893727617915793,
+          "rotation": 0,
+          "target": "1-master-bedroom-2-panorama-2"
+        },
+        {
+          "yaw": -1.7355950851389252,
+          "pitch": 0.7603896897992222,
+          "rotation": 0,
+          "target": "5-bedroom-panorama-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-master-bedroom-panorama",
+      "name": "master bedroom panorama",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6145303140878475,
+          "pitch": 0.6083147419153612,
+          "rotation": 0,
+          "target": "2-master-bedroom-panorama-2"
+        },
+        {
+          "yaw": -1.6175563415584158,
+          "pitch": 0.30150321600165597,
+          "rotation": 0,
+          "target": "5-bedroom-panorama-2"
+        },
+        {
+          "yaw": -1.612545015636659,
+          "pitch": 0.11994531686858068,
+          "rotation": 0,
+          "target": "1-master-bedroom-2-panorama-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-bedroom-panorama",
+      "name": "bedroom panorama",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8592024717683824,
+          "pitch": 0.5068492855982978,
+          "rotation": 0,
+          "target": "5-bedroom-panorama-2"
+        },
+        {
+          "yaw": 0.95200724218979,
+          "pitch": 0.2251604942047294,
+          "rotation": 0,
+          "target": "2-master-bedroom-panorama-2"
+        },
+        {
+          "yaw": 0.951626194050494,
+          "pitch": 0.06984786634392748,
+          "rotation": 0,
+          "target": "1-master-bedroom-2-panorama-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-bedroom-panorama-2",
+      "name": "bedroom panorama 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.006712734945608645,
+          "pitch": 0.4377336455778753,
+          "rotation": 0,
+          "target": "4-bedroom-panorama"
+        },
+        {
+          "yaw": -2.2224491247309555,
+          "pitch": 0.6751698258796281,
+          "rotation": 0,
+          "target": "1-master-bedroom-2-panorama-2"
+        },
+        {
+          "yaw": -2.2140180867330983,
+          "pitch": 0.8981414340732865,
+          "rotation": 0,
+          "target": "2-master-bedroom-panorama-2"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
